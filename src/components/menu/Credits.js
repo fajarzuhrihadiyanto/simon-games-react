fe07eacfdc379b365d120b44c65f2811f5877504
Fajar Zuhri Hadiyanto <52820619+fajarzuhrihadiyanto@users.noreply.github.com>
2021-10-27 09:19:14 +0700

    Fix misspelled word

--- src/components/menu/Credits.js
+++ src/components/menu/Credits.js
@@ -4,7 +4,7 @@ const Credits = () => {
   return (
     <Box>
       <Text align="justify" mt={4}>Music and sound effect by mixkit.co</Text>
-      <Text align="justify" mt={4}>UI by chackra-ui</Text>
+      <Text align="justify" mt={4}>UI by chakra-ui</Text>
       <Text align="justify" mt={4}>Build with react js</Text>
       <Text align="justify" mt={4}>Powered by vercel</Text>
     </Box>
